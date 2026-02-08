bfd3498c069ded5427a21a464800311e63e5e880
ryujimizutani <giroudozil11@gmail.com>
2026-02-08 20:26:43 +0900

    HTMLページなどをコミットします

---
+++ src/main/java/com/example/PassbookApp/index/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.PassbookApp.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "Start";
+    }
+}
